42227c232e52e1adeecb8bb421e89e15dd92f600
Clemens Dancso <79692576+lewonade@users.noreply.github.com>
2023-08-14 14:38:15 +0200

    Final Version
    you know the rules and so do i

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <div :class="{ 'is-invisible': hideWebsite }">
-    <div
-      id="app"
-      :class="{ 'is-invisible': !isPlaying && videoQueue.length === 0 }"
-    >
-      <div ref="playerDiv"></div>
+  <div>
+    <div :class="{ 'is-invisible': !isPlaying || isMusicVideo }">
+      <div id="app">
+        <div ref="playerDiv"></div>
+        <div class="progress" :style="{ width: playerWidth + 'px' }">
+          <div
+            class="progress-bar"
+            :style="{
+              width: progress + '%',
+              'background-color': progressBarColor,
+            }"
+          ></div>
+        </div>
+      </div>
+      <div class="twitch-username" v-if="currentVideo && currentVideo.username">
+        {{ currentVideo.username }}
+      </div>
     </div>
+
+    <div ref="emotePlayerDiv" class="is-invisible"></div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted, watch } from "vue";
+import { ref, onMounted } from "vue";
 import tmi from "tmi.js";
 
 export default {
@@ -22,22 +35,50 @@ export default {
     let currentVideo = null;
     let isVideoLoaded = false;
     const isPlaying = ref(false);
-    const hideWebsite = ref(false);
+    const progress = ref(0);
+    const playerWidth = ref(0);
+    const progressBarColor = ref("#3366ff");
+    const isMusicVideo = ref(false);
 
-    
-    const privilegedUser = "YOUR-TWITCH-USERNAME"; // Add a variable to store the privileged user that can use the chat commands
+    const updateProgress = () => {
+      if (currentVideo && player && player.getDuration) {
+        const currentTime = player.getCurrentTime();
+        const duration =
+          currentVideo.endTimeSeconds !== undefined
+            ? currentVideo.endTimeSeconds - currentVideo.startTimeSeconds
+            : player.getDuration();
+        const adjustedCurrentTime =
+          currentVideo.startTimeSeconds !== undefined
+            ? currentTime - currentVideo.startTimeSeconds
+            : currentTime;
+        progress.value = (adjustedCurrentTime / duration) * 100;
+        playerWidth.value = player.getIframe().clientWidth;
+      } else {
+        progress.value = 0;
+      }
+    };
 
-    const playNextVideo = () => {
-      if (videoQueue.value.length > 0 && player && !currentVideo) {
-        currentVideo = videoQueue.value[0];
-
+    const loadVideo = () => {
+      if (currentVideo && player) {
         player.loadVideoById({
           videoId: currentVideo.videoId,
-          startSeconds: currentVideo.startTimeSeconds,
+          startSeconds: currentVideo.startTimeSeconds || 0,
           endSeconds: currentVideo.endTimeSeconds,
         });
-
         isVideoLoaded = true;
+        progressBarColor.value =
+          "#" + Math.floor(Math.random() * 16777215).toString(16);
+      }
+    };
+
+    const playNextVideo = () => {
+      if (videoQueue.value.length > 0 && player) {
+        currentVideo = videoQueue.value.shift();
+        isMusicVideo.value = currentVideo.isMusic;
+        loadVideo();
+      } else {
+        isPlaying.value = false;
+        isVideoLoaded = false;
       }
     };
 
@@ -46,7 +87,6 @@ export default {
         player.stopVideo();
         currentVideo = null;
         isVideoLoaded = false;
-        videoQueue.value.shift();
         if (videoQueue.value.length === 0) {
           isPlaying.value = false;
         } else {
@@ -55,25 +95,29 @@ export default {
       }
     };
 
-    const stopPlaying = () => {
-      player.stopVideo();
-      isPlaying.value = false;
-      hideWebsite.value = true;
-    };
-
-    const startPlaying = () => {
-      hideWebsite.value = false;
-      skipToNextVideo(); // Skip the current video
-      playNextVideo(); // Play the next video
-    };
-
     const onPlayerStateChange = (event) => {
       if (event.data === window.YT.PlayerState.ENDED) {
-        skipToNextVideo();
-        playNextVideo(); // Play the next video after the current one ends
+        playNextVideo();
       } else if (event.data === window.YT.PlayerState.PLAYING) {
         isPlaying.value = true;
       }
+    };
+
+    const emoteVideoMap = {
+      lewonaHEHEHEHA: "12r5lYwIFPA",
+      lewonaBUHUHUHU: "RT_RzWrSrac",
+      lewonaGRRR: "GF0fG3KRhRo",
+      lewonaThumbs: "tjDS1PAqKUw",
+    };
+
+    const emotePlayerDiv = ref(null);
+    let emotePlayer;
+
+    const loadEmoteVideo = (videoId) => {
+      emotePlayer.loadVideoById({
+        videoId: videoId,
+      });
+      emotePlayer.playVideo();
     };
 
     onMounted(() => {
@@ -83,10 +127,29 @@ export default {
           width: "640",
           playerVars: {
             origin: window.location.origin,
-            autoplay: 1, // Enable autoplay
+            autoplay: 1,
+            controls: 0,
+            showinfo: 0,
+            rel: 0,
+            modestbranding: 1,
+            iv_load_policy: 3,
           },
           events: {
             onStateChange: onPlayerStateChange,
+          },
+        });
+
+        emotePlayer = new window.YT.Player(emotePlayerDiv.value, {
+          height: "0",
+          width: "0",
+          playerVars: {
+            origin: window.location.origin,
+            autoplay: 1,
+            controls: 0,
+            showinfo: 0,
+            rel: 0,
+            modestbranding: 1,
+            iv_load_policy: 3,
           },
         });
       };
@@ -94,9 +157,9 @@ export default {
       const options = {
         identity: {
           username: "YOUR-CLIENT-ID", // Go to this Link https://dev.twitch.tv/console/apps and add a new Applikation with the URL beeing 'https://localhost', everything else doesn't matter.
-          password: "oauth:abcdefghijklmnopqrstuvwxyz", // To retrieve your oautch-token you'll have to go to this website https://twitchapps.com/tmi/
+          password: "oauth:oauth:abcdefghijklmnopqrstuvwxyz", // To retrieve your oautch-token you'll have to go to this website https://twitchapps.com/tmi/
         },
-        channels: ["YOUR-TWITCH-USERNAME"], // Put in your Twitch-Username.
+        channels: ["Lewonade"], // Add you Twitch-Channel name. so the chatbot knows what chat to read.
       };
 
       const client = new tmi.Client(options);
@@ -108,76 +171,76 @@ export default {
 
         const command = message.toLowerCase();
         const isPrivilegedUser =
-          tags.username.toLowerCase() === privilegedUser.toLowerCase();
+          tags.badges &&
+          (tags.badges.vip || tags.badges.moderator || tags.badges.broadcaster);
 
-        if (command === "!skip" && isPrivilegedUser) {
+        if (
+          command === "!skip" &&
+          isPlaying.value &&
+          (isPrivilegedUser || tags.username === currentVideo.username)
+        ) {
           skipToNextVideo();
         } else if (command === "!pause" && isPrivilegedUser) {
           player.pauseVideo();
         } else if (command === "!resume" && isPrivilegedUser) {
           player.playVideo();
-        } else if (command === "!stop" && isPrivilegedUser) {
-          stopPlaying();
-        } else if (command === "!start" && isPrivilegedUser) {
-          startPlaying();
         } else {
           const isChannelPointReward =
-            tags["custom-reward-id"] === "YOUR-CUSTOM-REWARD-ID"; // You will have to create a channelpoint-reward on which the 'Viewer must enter text' is enabled.
-                                                                  // Use your channelpoint reward with a random word once and got to this website 'https://www.instafluff.tv/TwitchCustomRewardID/?channel=YOUR-TWITCH-USERNAME' <-- don't forget to edit the URL
+            tags["custom-reward-id"] === ""YOUR-CUSTOM-REWARD-ID"; // You will have to create a channelpoint-reward on which the 'Viewer must enter text' is enabled.
+          // Use your channelpoint reward with a random word once and got to this website 'https://www.instafluff.tv/TwitchCustomRewardID/?channel=YOUR-TWITCH-USERNAME' <-- don't forget to edit the URL
 
           if (isChannelPointReward) {
             const pattern =
-              /(\d{2}:\d{2}-\d{2}:\d{2})?\s?(https:\/\/(www\.|m\.)?youtube\.com\/(watch\?v=|shorts\/)?[A-Za-z0-9_-]+|https:\/\/youtu\.be\/[A-Za-z0-9_-]+)/;
+              /(\d{2}:\d{2}-\d{2}:\d{2})?\s*(https:\/\/)?(www\.)?youtube\.com\/(watch\?v=|shorts\/)(\S{11})/;
 
-            if (pattern.test(message)) {
-              const [, time, url] = message.match(pattern);
-              const [startTime, endTime] = (time || "00:00-00:00").split("-");
+            const matches = message.match(pattern);
 
-              let videoId;
-              if (url.includes("youtu.be")) {
-                videoId = url.split("/")[3];
-              } else if (url.includes("watch?v=")) {
-                videoId = url.split("=")[1];
-              } else if (url.includes("shorts/")) {
-                videoId = url.split("/")[4];
+            if (matches) {
+              const videoId = matches[5];
+              const timeRange = matches[1];
+              let startTimeSeconds, endTimeSeconds;
+
+              if (timeRange) {
+                const times = timeRange
+                  .split("-")
+                  .map((t) =>
+                    t.split(":").reduce((acc, time) => 60 * acc + +time)
+                  );
+                startTimeSeconds = times[0];
+                endTimeSeconds = times[1];
               }
 
-              const [startMinutes, startSeconds] = startTime.split(":");
-              const [endMinutes, endSeconds] = endTime.split(":");
-
-              const startTimeSeconds =
-                Number(startMinutes) * 60 + Number(startSeconds);
-              const endTimeSeconds =
-                Number(endMinutes) * 60 + Number(endSeconds);
-
               videoQueue.value.push({
-                videoId,
-                startTimeSeconds,
-                endTimeSeconds,
+                videoId: videoId,
+                username: tags.username,
+                startTimeSeconds: startTimeSeconds,
+                endTimeSeconds: endTimeSeconds,
+                isMusic: message.toLowerCase().includes("music"),
               });
 
-              if (!currentVideo && !isVideoLoaded && !hideWebsite.value) {
+              if (!isPlaying.value && !isVideoLoaded) {
                 playNextVideo();
               }
             }
           }
+
+          if (emoteVideoMap[message]) {
+            loadEmoteVideo(emoteVideoMap[message]);
+          }
         }
       });
 
-      player.addEventListener("onStateChange", onPlayerStateChange);
-    });
-
-    watch(videoQueue, () => {
-      if (!currentVideo && !isVideoLoaded && !hideWebsite.value) {
-        playNextVideo();
-      }
+      setInterval(updateProgress, 10);
     });
 
     return {
       playerDiv,
       isPlaying,
-      videoQueue,
-      hideWebsite,
+      progress,
+      playerWidth,
+      progressBarColor,
+      isMusicVideo,
+      emotePlayerDiv,
     };
   },
 };
@@ -186,5 +249,18 @@ export default {
 <style>
 .is-invisible {
   display: none;
+}
+.progress {
+  height: 15px;
+  background-color: #eee;
+  border-radius: 7px;
+  overflow: hidden;
+}
+.progress-bar {
+  height: 15px;
+  background-color: #3366ff;
+}
+#app {
+  padding: 20px;
 }
 </style>
